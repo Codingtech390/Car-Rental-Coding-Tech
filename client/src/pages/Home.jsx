005f9aef46c1110bd0cfc4d2de4eb21803a47420
codingtech <codingtech390@gmail.com>
2025-07-04 15:03:34 +0530

    Created Testimonial section. Next is newsLetter section for the homepage.

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,16 +1,18 @@
-import React from 'react'
-import Hero from '../components/Hero'
-import FeaturedSection from '../components/FeaturedSection'
-import Banner from '../components/Banner'
+import React from "react";
+import Hero from "../components/Hero";
+import FeaturedSection from "../components/FeaturedSection";
+import Banner from "../components/Banner";
+import Testimonial from "../components/Testimonial";
 
 const Home = () => {
   return (
     <>
-        <Hero />
-        <FeaturedSection />
-        <Banner />
+      <Hero />
+      <FeaturedSection />
+      <Banner />
+      <Testimonial />
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
